f8332fdd1915fa6edbfd74a1be476a61313fec2f
Christian Gellweiler <christian.gellweiler@1und1.de>
2016-07-06 23:33:46 +0200

    Now ES6 for real

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,7 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const path = require('path');
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import path from 'path';
 
-module.exports = {
+export default {
   entry: {
     main: ['./src/js/main.js', './src/css/main.scss'],
   },
