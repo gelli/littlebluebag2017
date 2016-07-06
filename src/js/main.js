ee590c12b1a995ed95a14071ef808558cf1289b3
Christian Gellweiler <christian.gellweiler@1und1.de>
2016-07-06 23:17:09 +0200

    Fix code to adhere to eslint rules :)

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@
 require('../css/main.scss');
 
 const WHO = 'JS';
-let greeter = (who) => 'Hello from ' + who + '!';
+const greeter = (who) => `Hello from ${who}!`;
 
 document.getElementById('app').appendChild(
     document.createTextNode(greeter(WHO))
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,35 @@
-/*jslint node: true */
-'use strict';
-
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    entry: {
-        main: ['./src/js/main.js', './src/css/main.scss']
-    },
-    output: {
-        path: require('path').resolve('assets'),
-        publicPath: '/',
-        filename: 'main.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
-            },
-            {
-                test: /\.scss$/,
-                loader:  ExtractTextPlugin.extract("style", "css?sourceMap!sass?sourceMap")
-            },
-        ]
-    },
-    resolve: {
-        modulesDirectories: ['./bower_components', 'node_modules']
-    },
-    plugins: [
-        new ExtractTextPlugin("[name].css")
-    ]
+  entry: {
+    main: ['./src/js/main.js', './src/css/main.scss'],
+  },
+  output: {
+    path: path.resolve('assets'),
+    publicPath: '/',
+    filename: 'main.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        },
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap!sass?sourceMap'),
+      },
+    ],
+  },
+  resolve: {
+    modulesDirectories: ['./bower_components', 'node_modules'],
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css'),
+  ],
 };
